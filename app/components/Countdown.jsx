e63b4cc54422ac3b317c69566cd7cdfe22862fa7
Sangeeta Jadoonanan <sjbitcode@gmail.com>
2018-04-13 01:38:23 -0400

    Created formatSeconds for Clock component. Add Clock tests for rendering and formatting seconds. Add styles to render Clock component

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,9 +1,10 @@
 var React = require('react');
+var Clock = require('Clock');
 
 var Countdown = () => {
     return (
         <div>
-            <p>Countdown.jsx</p>
+            <Clock totalSeconds={129}/>
         </div>
     );
 };
